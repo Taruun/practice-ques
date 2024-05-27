8dfba3d26aaae91385e8f643dd4ac20b520d9421
Taruun <tarunys2002@gmail.com>
2024-05-27 23:28:21 +0530

    que: B - Pasta

---
+++ B - Pasta .cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int n, m; cin >> n >> m;
+    int a[n], b[m];
+    
+    for(int i = 0; i < n; i++) cin >> a[i];
+    for(int i = 0; i < m; i++) cin >> b[i];
+    
+    bool ans = true;
+    for(int i = 0; i < m; i++){
+        bool flag = false;
+        for(int j = 0; j < n; j++){
+            if(a[j] == b[i]){
+                a[j] -= 1;
+                flag = true;
+                break;
+            }
+        }
+        if(!flag){
+            ans = false;
+            break;
+        }
+        
+    }
+    if (ans) cout << "Yes" << endl;
+    else cout << "No" << endl;
+    return 0;
+}
+
+#include <iostream>
+using namespace std;
+
+// WITHOUT ANS
+
+// int main() {
+//     int n, m; cin >> n >> m;
+//     int a[n], b[m];
+    
+//     for(int i = 0; i < n; i++) cin >> a[i];
+//     for(int i = 0; i < m; i++) cin >> b[i];
+    
+//     for(int i = 0; i < m; i++){
+//         bool flag = false;
+//         for(int j = 0; j < n; j++){
+//             if(a[j] == b[i]){
+//                 a[j] -= 1;
+//                 flag = true;
+//                 break;
+//             }
+//         }
+//         if(!flag){
+//             cout << "No" << endl;
+//             return 0;
+//         }
+        
+//     }
+//     cout << "Yes" << endl;
+//     return 0;
+// }
