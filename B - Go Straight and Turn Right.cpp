728bc5226d8c731b4137f483a032a9e3890a5eb4
Taruun <tarunys2002@gmail.com>
2024-05-30 00:28:41 +0530

    que: B - Go Straight and Turn Right

--- B - Go Straight and Turn Right.cpp
+++ B - Go Straight and Turn Right.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 using namespace std;
 
-
 int main() {
     int n; cin >> n;
     string s; cin >> s;
-    int x,y = 0;
     char dir = 'r';
+    int x = 0, y = 0;
     
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++){
         if(s[i] == 'S'){
             if(dir == 'r') x++;
             else if(dir == 'l') x--;
@@ -22,6 +21,7 @@ int main() {
             else dir = 'l';
         }
     }
+    
     cout << x << " " << y << endl;
     return 0;
 }
